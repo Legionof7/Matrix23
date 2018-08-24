17feea5b41229ca7447927543c6eb9116198df25
JZ <justin.zheng@gmail.com>
2018-08-23 20:19:33 -0700

    Description of outputs

--- FuzzyPoro.py
+++ FuzzyPoro.py
@@ -8,20 +8,22 @@ def FuzzyPoro():
 	BiometricData = "0123456789" #Sample Biometric Data
 	key, helper = extractor.generate(BiometricData) #Create the key and the helper
 
-	print key
+	print 'Your key is: %s' % (key)
 
  	KeyForTripleSec = TripleSec(key) #Runs your generated key through TripleSec
  	EncryptedPrivateKey = KeyForTripleSec.encrypt(b"ThisWouldBeYourPrivateKey") #Encrypts your private key
+
+ 	print 'Your encrypted private key is: %s' % (EncryptedPrivateKey)
 
 	KeyRecover = raw_input("Enter your key:") #The second time you scan your fingerprint/biometric data
 
 	KeyReturn = extractor.reproduce(KeyRecover, helper) #Creates your original key
 
-	print KeyReturn
+	print 'Your recovered key is: %s' % (KeyReturn)
 	
 	ExtractedKey = TripleSec(KeyReturn) #Runs your regenerated key through TripleSec
 	
-	
+	print 'Your private key is: '
 	print(ExtractedKey.decrypt(EncryptedPrivateKey).decode()) #Decodes your EncryptedPrivateKey with your regenerated encryption key
 
 
