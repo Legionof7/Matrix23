e7b006d1d68291ca5a8ba7ad608b328ab51682b9
Unknown <justin.zheng@gmail.com>
2018-09-16 13:43:39 -0700

    Fixes stuff and removes deprecated stuff

--- FuzzyPoroWithIFPS.py
+++ FuzzyPoroWithIFPS.py
@@ -5,15 +5,15 @@ import ipfsapi
 
 def FuzzyPoro():
 
-    extractor = FuzzyExtractor(10, 2)  # (CharacterLength, ErrorAllowed)
+    extractor = FuzzyExtractor(10, 3)  # (CharacterLength, ErrorAllowed)
     # Sample Biometric Data
-    BiometricData = input("Enter a biometric data (10 characters):")
+    BiometricData = input("Enter biometric data (10 characters):")
     BiometricData = BiometricData.encode('utf-8')
     PrivateKey = input("Enter your private key:")
     PrivateKey = PrivateKey.encode('utf-8')
     # Create the key and the helper
     key, helper = extractor.generate(BiometricData)
-
+  
     print ('Your key is: %s' % (key))
 
     # Runs your generated key through TripleSec
@@ -21,8 +21,7 @@ def FuzzyPoro():
     EncryptedPrivateKey = KeyForTripleSec.encrypt(
         PrivateKey)  # Encrypts your private key
 
-FuzzyPoro()
-IPFS()
+
     # This is very insecure obviously. But this is just for testing so :/
     FileWrite = open("EncryptedKey.txt", "w+")
     FileWrite.write(str(EncryptedPrivateKey))
@@ -32,7 +31,6 @@ IPFS()
 
     # The second time you scan your fingerprint/biometric data
     KeyRecover = input("Enter your biometric data again:")
-
     KeyReturn = extractor.reproduce(
         KeyRecover, helper)  # Creates your original key
 
